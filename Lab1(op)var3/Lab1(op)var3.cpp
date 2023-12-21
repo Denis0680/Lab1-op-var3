d82ca5dc353fa7478f34fb83aba995b0c06d1c57
777 <777@DESKTOP-90ENVDG>
2023-12-21 13:58:45 +0200

    Add project files.

---
+++ Lab1(op)var3/Lab1(op)var3.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a, b, c, min;
+	cout << "a,b,c= ";
+	cin >> a >> b >> c;
+
+	if (a < b && a < c) {
+		min = a;
+	}
+	else if (b < a && b < c) {
+		min = b;
+	}
+	else if (c < b && c < a) {
+		min = c;
+	}
+
+	cout << "min(a,b,c)= " << min;
+}
